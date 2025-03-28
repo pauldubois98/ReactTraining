b270c12e483a2fe204b33c8519a6e179e01f3e23
pauldubois98 <pauldubois98@gmail.com>
2025-03-28 11:21:27 +0100

    structure

--- connect_x/src/App.jsx
+++ connect_x/src/App.jsx
@@ -1,8 +1,16 @@
+import React from "react";
 import { useState } from "react";
 import "./App.css";
+import Grid from "./Grid";
+import Form from "./Form";
 
 function App() {
-    return <></>;
+    return (
+        <React.Fragment>
+            <Form />
+            <Grid />
+        </React.Fragment>
+    );
 }
 
 export default App;
