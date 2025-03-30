add0501da66ffe300cb1077019dfffefb79bdd90
pauldubois98 <pauldubois98@gmail.com>
2025-03-30 17:25:19 +0200

    winner move func

--- connect_x/src/utils.js
+++ connect_x/src/utils.js
@@ -21,3 +21,27 @@ export function empty(board) {
     }
     return c == 0;
 }
+
+export function winnerMove(val, board, i, N, M, X) {
+    const DIRECTONS = [1, N, N + 1, N - 1];
+    console.log(val, i, board, M, N, X);
+    for (let d of DIRECTONS) {
+        var c = 1;
+        var j = i + d;
+        while (j < board.length && board[j] == val) {
+            j += d;
+            c++;
+        }
+        var j = i - d;
+        while (j < board.length && board[j] == val) {
+            j -= d;
+            c++;
+        }
+        // console.log(d, c, X);
+        if (c >= X) {
+            console.log("True");
+            return true;
+        }
+    }
+    return false;
+}
