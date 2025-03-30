dd0dabe8ab317373eb085e9672913bb9914a63bf
pauldubois98 <pauldubois98@gmail.com>
2025-03-30 01:13:13 +0100

    is board empty func

--- connect_x/src/utils.js
+++ connect_x/src/utils.js
@@ -11,3 +11,13 @@ export function turn(board) {
         return 1;
     }
 }
+
+export function empty(board) {
+    var c = 0;
+    for (var i = 0; i < board.length; i++) {
+        if (board[i] == 0) {
+            c++;
+        }
+    }
+    return c == 0;
+}
